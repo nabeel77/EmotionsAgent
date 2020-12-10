712ea8102f6f0ec56aa72d68193a5570d4a43ebb
nabeel <nabeel.beeni30@gmail.com>
2020-12-10 02:03:32 +0200

    Implemented different function to define each model

--- Models.py
+++ Models.py
@@ -5,159 +5,110 @@ from keras.layers import MaxPooling2D
 from keras.layers import Convolution2D
 from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 
-def get_model1():
-  model = Sequential()
+num_features = 64
+num_classes = 5
+img_size, batch_size = 48, 64
+# EPOCHS = 50
+DROPOUT_RATE = 0.5
+FROZEN_LAYER_NUM = 170
 
-  model.add(Convolution2D(64, (3, 3), padding='same', input_shape=(48,48,1)))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+ADAM_LEARNING_RATE = 0.001
+SGD_LEARNING_RATE = 0.01
+SGD_DECAY = 0.0001
+Resize_pixelsize = 197
 
-  model.add(Convolution2D(128, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
 
-  model.add(Convolution2D(256, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+def FERC2():
+    # https://github.com/NJNischal/Facial-Expression-Recognition-with-CNNs/blob/master/Facial_Expression_Training.ipynb
 
-  model.add(Convolution2D(512, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+    model = Sequential()
 
-  model.add(Flatten())
+    # Conv Block 1
+    model.add(Conv2D(64, (3, 3), padding='same', input_shape=(48, 48, 1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-  model.add(Dense(512))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Dropout(0.25))
+    # Conv Block 2
+    model.add(Conv2D(128, (5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-  model.add(Dense(256))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Dropout(0.25))
+    # Conv Block 3
+    model.add(Conv2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-  model.add(Dense(6))
-  model.add(Activation('softmax'))
+    # Conv Block 4
+    model.add(Conv2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.3))
 
-  return model
+    # Conv Block 5
+    model.add(Conv2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.5))
 
-# actual model
-def get_model2():
-  model = Sequential()
+    model.add(Flatten())
 
-  model.add(Convolution2D(64, (3, 3), padding='same', input_shape=(48,48,1)))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+    # Fully connected Block 1
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
 
-  model.add(Convolution2D(128, (5, 5), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+    # Fully connected Block 2
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
 
-  model.add(Convolution2D(512, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+    model.add(Dense(5, activation='softmax'))
 
-  model.add(Convolution2D(512, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+    return model
 
-  model.add(Flatten())
 
-  model.add(Dense(256))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Dropout(0.25))
+def FERC():
+    # https://github.com/chinhau-lim/fer_2013/blob/master/FER_2013.ipynb
 
-  model.add(Dense(512))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Dropout(0.25))
+    model = Sequential()
 
-  model.add(Dense(6))
-  model.add(Activation('softmax'))
+    # 1. Conv
+    model.add(Conv2D(64, (3, 3), padding='same', input_shape=(48, 48, 1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-  return model
+    # 2. Conv Layer
+    model.add(Conv2D(128, (5, 5), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-# deeper model
-def get_model3():
-  model = Sequential()
+    # 3. Conv Layer
+    model.add(Conv2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-  model.add(Convolution2D(64, (3, 3), padding='same', input_shape=(48,48,1)))
-  model.add(Convolution2D(64, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.4))
-
-  model.add(Convolution2D(128, (3, 3), padding='same'))
-  model.add(Convolution2D(128, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.4))
-
-  model.add(Convolution2D(256, (3, 3), padding='same'))
-  model.add(Convolution2D(265, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.4))
-
-  model.add(Convolution2D(512, (3, 3), padding='same'))
-  model.add(Convolution2D(512, (3, 3), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.4))
-
-  model.add(Flatten())
-
-  model.add(Dense(2048))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Dropout(0.4))
-
-  model.add(Dense(512))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(Dropout(0.4))
-
-  model.add(Dense(6))
-  model.add(Activation('softmax'))
-
-  return model
-
-# shallow model
-def get_model4():
-  model = Sequential()
-
-  model.add(Convolution2D(64, (3, 3), padding='same', input_shape=(48,48,1)))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
-
-  model.add(Convolution2D(128, (5, 5), padding='same'))
-  model.add(BatchNormalization())
-  model.add(Activation('relu'))
-  model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
-  model.add(Dropout(0.25))
+    # 4. Conv Layer
+    model.add(Conv2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
   model.add(Flatten())
 
@@ -166,17 +117,90 @@ def get_model4():
   model.add(Activation('relu'))
   model.add(Dropout(0.25))
 
-  model.add(Dense(6))
-  model.add(Activation('softmax'))
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
 
-  return model
+    model.add(Dense(5, activation='softmax'))
+    return model
+
+
+def ResNet50():
+    vgg_notop = VGGFace(model='resnet50', include_top=False, input_shape=(Resize_pixelsize, Resize_pixelsize, 3),
+                        pooling='avg')
+    last_layer = vgg_notop.get_layer('avg_pool').output
+    x = Flatten(name='flatten')(last_layer)
+    x = Dropout(DROPOUT_RATE)(x)
+    x = Dense(4096, activation='relu', name='fc6')(x)
+    x = Dropout(DROPOUT_RATE)(x)
+    x = Dense(1024, activation='relu', name='fc7')(x)
+    x = Dropout(DROPOUT_RATE)(x)
+    # l=0
+    # for layer in vgg_notop.layers:
+    #     print(layer,"["+str(l)+"]")
+    #     l=l+1
+
+    batch_norm_indices = [2, 6, 9, 13, 14, 18, 21, 24, 28, 31, 34, 38, 41, 45, 46, 53, 56, 60, 63, 66, 70, 73, 76, 80,
+                          83,
+                          87, 88, 92, 95, 98, 102, 105, 108, 112, 115, 118, 122, 125, 128, 132, 135, 138, 142, 145, 149,
+                          150, 154, 157, 160, 164, 167, 170]
+    for i in range(FROZEN_LAYER_NUM):
+        if i not in batch_norm_indices:
+            vgg_notop.layers[i].trainable = False
+    out = Dense(5, activation='softmax', name='classifier')(x)
+    model = Model(vgg_notop.input, out)
+
+    return model
+
+
+def Proposed_model():
+    model = Sequential()
+
+    model.add(Convolution2D(64, (3, 3), padding='same', input_shape=(48, 48, 1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(128, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(256, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(512, (3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same'))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(256))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(5))
+    model.add(Activation('softmax'))
+
+    return model
+
+
 if __name__ == '__main__':
-      model1 = get_model1()
-      model2 = get_model2()
-      model3 = get_model3()
-      model4 = get_model4()
-
-      model1.summary()
-      model2.summary()
-      model3.summary()
-      model4.summary()
+    FERC2()
+    FERC()
+    ResNet50()
+    Proposed_model()
