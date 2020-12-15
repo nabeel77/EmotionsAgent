4f373654627aafd6bc450b8f2f1c04739a9e4310
nabeel <nabeel.beeni30@gmail.com>
2020-12-16 01:37:51 +0200

    Added separate threads to run face, emotion detection and the voice agent

--- EmotionDetection.py
+++ EmotionDetection.py
@@ -9,23 +9,28 @@ import ctypes
 import cv2
 import time
 import argparse
+import threading
+import concurrent.futures
 import numpy as np
 import TrainingModels as tm
 from threading import Thread
 import PersonalAssistant as pa
 from keras.models import load_model
+import multiprocessing
 from multiprocessing import Process, Array, Value
 from keras.preprocessing.image import img_to_array
 
+WAKE_WORD = 'hey bro'
 height, width = 48, 48
 batch_size = 64
 validation_data_dir = './fer2013/validation'
-
-classifier = load_model('./model3.h5')
+label = ''
+# wake = multiprocessing.Value('i', 0)
+wake = ''
+classifier = load_model('./model2.h5')
 print('model loaded')
 
 validation_generator = tm.get_datagen(validation_data_dir)
-
 class_labels = validation_generator.class_indices
 class_labels = {v: k for k, v in class_labels.items()}
 classes = list(class_labels.values())
@@ -51,61 +56,30 @@ def face_detector_color(img):
     return (x, w, y, h), roi_color, img
 
 
-# def face_detector(img):
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-#     if faces is ():
-#         return (0, 0, 0, 0), np.zeros((48, 48), np.uint8), img
-#
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#         roi_gray = gray[y:y + h, x:x + w]
-#
-#     try:
-#         roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-#     except:
-#         return (x, w, y, h), np.zeros((48, 48), np.uint8), img
-#     return (x, w, y, h), roi_gray, img
+def face_detector(img):
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return (0, 0, 0, 0), np.zeros((48, 48), np.uint8), img
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+
+    try:
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+    except:
+        return (x, w, y, h), np.zeros((48, 48), np.uint8), img
+    return (x, w, y, h), roi_gray, img
+
+
 name = 'Hello Nabeel, how are you'
 
 
-def startSoundThread():
-    mus = Thread(target=pa.speak, args=[name])
+def startSoundThread(sentence):
+    mus = Thread(target=pa.speak, args=[sentence])
     mus.start()
-
-
-# def make_prediction():
-#     cap = cv2.VideoCapture(0)
-#     #confirmation = input('type yes if you want to detect the expressions: ')
-#     counter = 1
-#     while cap.isOpened():
-#         while True:
-#             ret, frame = cap.read()
-#             rect, face, image = face_detector(frame)
-#             emotion_detector(rect,face,image)
-#             Show_cam(image)
-#             if counter == 1:
-#                 startSoundThread()
-#                 #text = pa.get_audio()
-#                 #print(text)
-#                 counter = -1
-#             # if counter == -1:
-#             #     q = queue.Queue()
-#             #     listener = Thread(target=pa.get_audio, args=(q,)).start()
-#             #     result = q.get()
-#             #     print(result)
-#             #     counter = 2
-#             #Show_cam(image)
-#             if cv2.waitKey(1) == 13:  # 13 is the Enter Key
-#                 break
-#             # else:
-#             #     Show_cam(image)
-#             #     if cv2.waitKey(1) == 13:  # 13 is the Enter Key
-#             #         break
-#         cap.release()
-#         cv2.destroyAllWindows()
-#     else:
-#         pa.speak('Camera is disconnected. Good bye.')
 
 
 def emotion_detector(rect, face, image):
@@ -114,143 +88,55 @@ def emotion_detector(rect, face, image):
         roi = face.astype("float") / 255.0
         roi = img_to_array(roi)
         roi = np.expand_dims(roi, axis=0)
-
         # make a prediction on the video image, then lookup the class
         preds = classifier.predict(roi)[0]
         label = class_labels[preds.argmax()]
-        #print(label)
         label_position = (rect[0] + int((rect[1] / 2)), rect[2] + 25)
         cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
     else:
         cv2.putText(image, "No Face Found", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
     return label
 
-class VideoCapture:
-    """
-    Class that handles video capture from device or video file
-    """
-    def __init__(self, device=0, delay=0.):
-        """
-        :param device: device index or video filename
-        :param delay: delay between frame captures in seconds(floating point is allowed)
-        """
-        self._cap = cv2.VideoCapture(device)
-        self._delay = delay
 
-    def _proper_frame(self, delay=None):
-        """
-        :param delay: delay between frames capture(in seconds)
-        :param finished: synchronized wrapper for int(see multiprocessing.Value)
-        :return: frame
-        """
-        snapshot = None
-        correct_img = False
-        fail_counter = -1
-        while not correct_img:
-            # Capture the frame
-            correct_img, snapshot = self._cap.read()
-            fail_counter += 1
-            # Raise exception if there's no output from the device
-            if fail_counter > 10:
-                raise Exception("Capture: exceeded number of tries to capture the frame.")
-            # Delay before we get a new frame
-            time.sleep(delay)
-        return snapshot
-
-    def get_size(self):
-        """
-        :return: size of the captured image
-        """
-        return (int(self._cap.get(int(cv2.CAP_PROP_FRAME_HEIGHT))),
-                int(self._cap.get(int(cv2.CAP_PROP_FRAME_WIDTH))), 3)
-
-    def get_stream_function(self):
-        """
-        Returns stream_function object function
-        """
-
-        def stream_function(image, finished):
-            """
-            Function keeps capturing frames until finished = 1
-            :param image: shared numpy array for multiprocessing(see multiprocessing.Array)
-            :param finished: synchronized wrapper for int(see multiprocessing.Value)
-            :return: nothing
-            """
-            # Incorrect input array
-            if image.shape != self.get_size():
-                raise Exception("Capture: improper size of the input image")
-            print("Capture: start streaming")
-            # Capture frame until we get finished flag set to True
-            while not finished.value:
-                image[:, :, :] = self._proper_frame(self._delay)
-            # Release the device
-            self.release()
-
-        return stream_function
-
-    def release(self):
-        self._cap.release()
-
-def main():
-    # Add program arguments
-    parser = argparse.ArgumentParser(description='Captures the video from the webcamera and \nwrites it into the output file with predefined fps.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-output', dest="output",  default="output.avi", help='name of the output video file')
-    parser.add_argument('-log', dest="log",  default="frames.log", help='name of the log file')
-    parser.add_argument('-fps', dest="fps",  default=25., help='frames per second value')
-
-    # Read the arguments if any
-    result = parser.parse_args()
-    fps = float(result.fps)
-    output = result.output
-    log = result.log
-
-    # Initialize VideoCapture object and auxilary objects
-    cap = VideoCapture()
-    shape = cap.get_size()
-    stream = cap.get_stream_function()
-
-    # Define shared variables(which are synchronised so race condition is excluded)
-    shared_array_base = Array(ctypes.c_uint8, shape[0] * shape[1] * shape[2])
-    frame = np.ctypeslib.as_array(shared_array_base.get_obj())
-    frame = frame.reshape(shape[0], shape[1], shape[2])
-    finished = Value('i', 0)
-
-    # Start processes which run in parallel
-    video_process = Process(target=stream, args=(frame, finished))
-    video_process.start()  # Launch capture process
-
-    # Sleep for some time to allow videocapture start working first
-    time.sleep(2)
-
-    # Termination function
-    def terminate():
-        print("Main: termination")
-        finished.value = True
-        # Wait for all processes to finish
-        time.sleep(1)
-        # Terminate working processes
-        video_process.terminate()
-
-    # The capturing works until keyboard interrupt is pressed.
-    counter = 1
+def make_prediction():
+    global label
+    global wake
+    cap = cv2.VideoCapture(0)
+    print('name is: ', threading.current_thread().name)
+    print(threading.get_ident())
     while True:
-        try:
-            # Display the resulting frame
-            rect, face, image = face_detector_color(frame)
-            emotion_detector(rect, face, image)
-            if counter == 1:
-                startSoundThread()
-                p = Thread(target=pa.get_audio)
-                p.start()
-                counter = -1
-            cv2.imshow('frame', frame)
-            cv2.waitKey(1)  # Display it at least one ms before going to the next frame
-            time.sleep(0.1)
-
-        except KeyboardInterrupt:
-            cv2.destroyAllWindows()
-            terminate()
+        ret, frame = cap.read()
+        rect, face, image = face_detector(frame)
+        if wake == 'hey bro':
+            label = emotion_detector(rect, face, image)
+        cv2.imshow('All', image)
+        if cv2.waitKey(1) == 13:
             break
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
-    main()
+    # print('name is: ', threading.current_thread().name)
+    # print(threading.get_ident())
+    m = Thread(target=make_prediction, args=())
+    m.start()
+    time.sleep(3)
+    while True:
+        print('Listening')
+        text = pa.get_audio()
+        if text.count(WAKE_WORD) > 0:
+            wake = 'hey bro'
+            print(f'i am {wake}')
+            # speak_thread = Process(target=pa.speak, args=(label,)).start()
+            speak_thread = Thread(target=pa.speak, args=[label])
+            speak_thread.start()
+            speak_thread.join()
+        elif text == 'stop':
+            wake = 'stopping'
+            speak_thread = Thread(target=pa.speak, args=[text])
+            speak_thread.start()
+            speak_thread.join()
+            break
+    # for t in threading.enumerate():
+    #         print(t.name)
