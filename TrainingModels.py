0f76f464800e36be1f2f3ca94fc9e09e1f07a023
nabeel <nabeel.beeni30@gmail.com>
2021-01-01 21:37:27 +0200

    Models separated in different functions

--- TrainingModels.py
+++ TrainingModels.py
@@ -1,6 +1,15 @@
+'''
+Things done in this file:
+1. Import models from Models
+2. Apply data augmentation
+3. Compile the models
+4. Train the models
+'''
+
+import pickle
 import Models
-from keras.regularizers import l2
-from keras.optimizers import RMSprop, SGD, Adam
+from keras.optimizers import Adam
+from livelossplot import PlotLossesKeras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 
@@ -9,20 +18,21 @@ validation_data_dir = './fer2013/validation'
 BS = 128
 Resize_pixelsize = 197
 
-# Using data augmentation
-train_datagen = ImageDataGenerator(
-    featurewise_center=False,
-    samplewise_center=False,
-    featurewise_std_normalization=False,
-    samplewise_std_normalization=False,
-    zca_whitening=False,
-    rotation_range=0,
-    width_shift_range=0.0,
-    height_shift_range=0.0,
-    shear_range=0,
-    zoom_range=0.0,
-    horizontal_flip=True,
-    vertical_flip=False)
+
+# Using data augmentationa
+def get_datagen(dataset, modelType='grayscale', aug=False):
+    if aug:
+        datagen = ImageDataGenerator(
+            rescale=1. / 255,
+            featurewise_center=False,
+            featurewise_std_normalization=False,
+            rotation_range=10,
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            zoom_range=0.1,
+            horizontal_flip=True)
+    else:
+        datagen = ImageDataGenerator(rescale=1. / 255)
 
     if modelType == 'TransferLearning':
         return datagen.flow_from_directory(
@@ -57,27 +67,42 @@ if __name__ == '__main__':
     model4.compile(optimizer=Adam(lr=0.0005), loss='categorical_crossentropy', metrics=['accuracy'])
     model4.summary()
 
-    nb_train_samples = 28273
-    nb_validation_samples = 3534
-    epochs = 50
-
-    history1 = Models.get_model1().fit_generator(
-        train_generator,
-        steps_per_epoch=nb_train_samples // batch_size,
-        epochs=epochs,
-        verbose=1,
-        callbacks=callbacks,
-        validation_data=validation_generator,
-        validation_steps=nb_validation_samples // batch_size)
-
-    history2 = Models.model2.fit_generator(
-        train_generator,
-        steps_per_epoch=nb_train_samples // batch_size,
-        epochs=epochs,
-        verbose=1,
-        callbacks=callbacks,
-        validation_data=validation_generator,
-        validation_steps=nb_validation_samples // batch_size)
+    # training models
+    train_generator = None
+    validation_generator = None
+    for i in range(4):
+        path = "./Model" + i + ".h5"
+        checkpoint = ModelCheckpoint(path, monitor='val_accuracy', save_best_only=True, mode='max', verbose=1)
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, min_lr=0.00001, model='auto')
+        callbacks = [PlotLossesKeras(), checkpoint, reduce_lr]
+        if i == 2:
+            train_generator = get_datagen(train_data_dir, 'TransferLearning', True)
+            validation_generator = get_datagen(validation_data_dir, 'TransferLearning')
+            history_resnet50 = model3.fit_generator(
+                generator=train_generator,
+                validation_data=validation_generator,
+                steps_per_epoch=24176 // BS,
+                validation_steps=3006 // BS,
+                shuffle=True,
+                epochs=50,
+                callbacks=callbacks,
+                use_multiprocessing=True,
+            )
+            with open('./ResNt50_History', 'wb') as file_pi:
+                pickle.dump(history_resnet50.history, file_pi)
+        else:
+            train_generator = get_datagen(train_data_dir, aug=True)
+            validation_generator = get_datagen(validation_data_dir)
+            history_FERC2 = model1.fit(
+                x=train_generator,
+                steps_per_epoch=24176 // BS,
+                epochs=50,
+                validation_data=validation_generator,
+                validation_steps=3006 // BS,
+                callbacks=callbacks
+            )
+            with open('./FERC_History', 'wb') as file_pi:
+                pickle.dump(history_FERC2.history, file_pi)
 
             history_FERC2 = model2.fit(
                 x=train_generator,
@@ -90,11 +115,14 @@ if __name__ == '__main__':
             with open('./FERC2_History', 'wb') as file_pi:
                 pickle.dump(history_FERC2.history, file_pi)
 
-    history4 = Models.model4.fit_generator(
-        train_generator,
-        steps_per_epoch=nb_train_samples // batch_size,
-        epochs=epochs,
-        verbose=1,
-        callbacks=callbacks,
-        validation_data=validation_generator,
-        validation_steps=nb_validation_samples // batch_size)
+            history_Proposed_Model = model4.fit(
+                x=train_generator,
+                steps_per_epoch=24176 // BS,
+                epochs=50,
+                validation_data=validation_generator,
+                validation_steps=3006 // BS,
+                callbacks=callbacks
+            )
+            # saving history
+            with open('./Proposed_Model_history', 'wb') as file_pi:
+                pickle.dump(history_Proposed_Model.history, file_pi)
